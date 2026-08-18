2dc5477a1fc2edf5dd58779e7f28a24369756709
sanskar-pranik <sanskar@pranik.ai>
2026-08-19 01:58:08 +0530

    added non negative constraints

--- services/product-service/prisma/migrations/20260818201558/migration.sql
+++ services/product-service/prisma/migrations/20260818201558/migration.sql
@@ -9,7 +9,9 @@ CREATE TABLE "products" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "products_price_paise_check" CHECK ("price_paise" >= 0),
+    CONSTRAINT "products_stock_check" CHECK ("stock" >= 0)
 );
 
 -- CreateIndex
